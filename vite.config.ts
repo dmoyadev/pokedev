442162fa3276514004faa5a957ffb3f78afe7f36
Daniel Moya Leiva <danielmoyaleiva@gmail.com>
2022-08-31 13:55:08 +0200

    🏗 Change base URL for deployments

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === 'production' ? '/pokedev/' : '/',
-
 	plugins: [
 		vue()
 	],
